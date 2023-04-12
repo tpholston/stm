17c4ffaa8a37a40d924df6ff179c50c208bbeb9e
tpholston <tholston@ucsd.edu>
2023-04-12 15:48:38 -0700

    Update __init__.py

--- src/py_stm/__init__.py
+++ src/py_stm/__init__.py
@@ -1,6 +1,10 @@
 """
-This package name is a Python package for implementing the Structural Topic Model (STM), a statistical method for analyzing topic patterns in text data. 
-It provides tools for ingesting and manipulating text data, estimating Structural Topic Models, calculating covariate effects on latent topics with uncertainty, estimating topic correlations, and creating visualizations for analysis.
+This package name is a Python package for implementing the Structural Topic Model (STM), 
+a statistical method for analyzing topic patterns in text data. 
+
+It provides tools for ingesting and manipulating text data, estimating Structural Topic Models, 
+calculating covariate effects on latent topics with uncertainty, estimating topic correlations, 
+and creating visualizations for analysis.
 """
 
 # bring model classes directly into package namespace, to save some typing
